6363690c78fd284f31162aa17708563b6e0795b8
beetayang <byang75@wisc.edu>
2017-11-18 12:26:17 -0600

    Finish Counting Letters Project

---
+++ countingletters.py
@@ -0,0 +1,24 @@
+'''
+Created on Nov 18, 2017
+
+@author: ITAUser
+'''
+#crate a function that accepts the filename and character
+#open the text file and read
+from constitution import text
+from itertools import count
+    f = open(filename, 'r')
+    count = 0;
+    for c in text
+        if c == 'a':
+            count = count + 1
+        return count
+    print (countA("banana"))
+#read the file 
+#split the file
+#count the letters
+#print result
+
+
+def countingletters(filename, mychar):
+    
